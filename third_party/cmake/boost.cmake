a0615c8ff349a1ae77c752fd3a90a61284d432e3
Jarrett Knauer <jrtknauer@gmail.com>
2022-09-21 21:51:46 -0600

    build: update boost requirements

--- third_party/cmake/boost.cmake
+++ third_party/cmake/boost.cmake
@@ -1,2 +1,12 @@
-# Boost::process:v2 is an experimental release
-find_package(Boost 1.80.0 REQUIRED)
+# Boost::process:v2 is an experimental release - but header only
+set(Boost_USE_DEBUG_LIBS OFF)
+set(Boost_USE_RELEASE_LIBS ON)
+
+find_package(
+    Boost
+    1.80.0
+    REQUIRED)
+
+find_package(
+    Threads
+    REQUIRED)
